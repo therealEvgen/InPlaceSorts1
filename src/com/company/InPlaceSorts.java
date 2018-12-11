a479e11cf8276474f32036a537e45ba62ebae1e6
BT_1N3_23 <ekorsunsky0407@bths.edu>
2018-12-11 11:28:53 -0500

    done

--- src/com/company/InPlaceSorts.java
+++ src/com/company/InPlaceSorts.java
@@ -1,139 +1,184 @@
 package com.company;
 
-public class InPlaceSorts {
-    public static void mergeSort(int[] arr) {
-        int n = arr.length;
-        int[] temp = new int[n];
-        mergeSortHelper(arr, 0, n - 1, temp);
-    }
+public class  InPlaceSorts {
 
-    public static void mergeSortHelper(int[] arr, int left, int right, int[] temp) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSortHelper(arr, left, mid, temp);
-            mergeSortHelper(arr, mid + 1, right, temp);
-            merge(arr, left, mid, right, temp);
+    public  void insertionSort(int[] list1)
+    {
+        for (int i=1;i<list1.length;i++)
+        {
+            int current = list1[i];
+            int x = i-1;
+
+            while (x>=0 && list1[x]>current) // moves all of the numbers before the key right to make space.
+            {
+                list1[x+1] = list1[x];
+                x=x-1;
+            }
+            list1[x+1] = current;
         }
     }
 
-    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
-        int i = left;
+    public  void selectionSort (double list2[])
+    {
+
+        for (int i = 0; i < list2.length-1; i++)
+        {
+            int min = i;
+
+            for (int x = i+1; x < list2.length; x++)
+                if (list2[x] < list2[min])
+                    min = x;
+
+            swapDouble(list2,min,i);
+        }
+
+
+    }
+
+
+    public  void bubbleSort(String[] list3) {
+        for (int i = 0; i < list3.length; i++) {
+            for (int j = i + 1; j < list3.length; j++) {
+                if ((list3[i].compareTo(list3[j])) > 0) {
+                    swapString(list3,i,j);
+                }
+            }
+        }
+    }
+    public int[] randomIntArr(int count)
+    {
+        int[] arr =new int[count];
+        for (int i=0;i<arr.length;i++)
+        {
+            arr[i]=(int)(Math.random()*10000);
+        }
+        return arr;
+    }
+
+
+
+
+    public double [] IntToDouble (int [] arr)
+    {
+        double[] doublearr = new double[arr.length];
+        for (int x=0;x<arr.length;x++)
+        {
+            double temp = (arr[x]);
+            doublearr[x] = temp;
+        }
+        return doublearr;
+    }
+
+    public String [] IntToString (int [] arr) {
+        String[] stringarr = new String[arr.length];
+        for (int x = 0; x < arr.length; x++) {
+            stringarr[x] = Integer.toString(arr[x]);
+        }
+        return stringarr;
+    }
+
+    public static void swapString(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void swapDouble(double[] arr, int i, int j) {
+        Double temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void mergeSort(int[] elements)
+    {
+        int n = elements.length;
+        int[] temp = new int[n];
+        mergeSortHelper(elements, 0, n-1, temp);
+    }
+
+    private static void mergeSortHelper(int[] elements,
+                                        int from, int to, int[] temp)
+    {
+        if (from < to)
+        {
+            int middle = (from + to) / 2;
+            mergeSortHelper(elements, from, middle, temp);
+            mergeSortHelper(elements, middle + 1, to, temp);
+            merge(elements, from, middle, to, temp);
+        }
+    }
+
+    private static void merge(int[] elements, int from, int mid, int to, int[] temp)
+    {
+        int i = from;
         int j = mid + 1;
-        int k = left;
-        while (i <= mid && j <= right) {
-            if (arr[i] < arr[j]) {
-                temp[k] = arr[i];
+        int k = from;
+        while (i <= mid && j <= to)
+        {
+            if (elements[i] < elements[j])
+            {
+                temp[k] = elements[i];
                 i++;
-            } else {
-                temp[k] = arr[j];
+            }
+            else
+            {
+                temp[k] = elements[j];
                 j++;
             }
             k++;
         }
-        while (i <= mid) {
-            temp[k] = arr[i];
+        while (i <= mid)
+        {
+            temp[k] = elements[i];
             i++;
             k++;
         }
-        while (j <= right) {
-            temp[k] = arr[j];
+        while (j <= to)
+        {
+            temp[k] = elements[j];
             j++;
             k++;
         }
-        for (k = left; k <= right; k++) {
-            arr[k] = temp[k];
+        for (k = from; k <= to; k++) {
+            elements[k] = temp[k];
         }
     }
 
-    public static void selectionSort(double[] list1) {
-        for (int i = 0; i < list1.length; i++) {
-            double curMin = list1[i];
-
-            for (int j = i+1; j < list1.length; j++) {
-                if (list1[j] < curMin) {
-                    curMin = list1[j];
-                    swapD(list1,i,j);
-                }
-            }
-        }
-
-    public static void bubbleSort(String[] list1) {
-        int swaps = 1;
-        while (swaps!=0) {
-            swaps = 0;
-            for (int i = 0; i < list1.length-1; i++) {
-                if (list1[i].compareTo(list1[i+1]) > 0) {
-                    swapArr(list1, i, i+1);
-                    swaps++;
-                }
-            }
-        }
+    public static void swap(int[] arr, int index1, int index2)
+    {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 
-    public class InsertionSort(int[] list1) {
-            void Insertionsort(int list1[])
+    public static int partition(int[] arr, int left, int right)
+    {
+        int pivot = arr[right];
+        int i = left - 1;
+
+        for(int j = left; j < right; j++)
         {
-            int n = list1.length;
-            for (int i=1; i<n; ++i)
+            if (arr[j] <= pivot)
             {
-                int key = list1[i];
-                int j = i-1;
-                while (j>=0 && list1[j] > key)
-                {
-                    list1[j+1] = list1[j];
-                    j = j-1;
-                }
-                list1[j+1] = key;
-            }
-        }
-    }
-    public static void swapI(int list1[], int i, int j){
-        int temp = list1[i];
-        list1[i]=list1[j];
-        list1[j]=temp;
-    }
-    public static void swapD(double list1[], int i, int j){
-        double temp = list1[i];
-        list1[i]=list1[j];
-        list1[j]=temp;
-    }
-    public static void swapS(String list1[], int i, int j){
-        String temp = list1[i];
-        list1[i]=list1[j];
-        list1[j]=temp;
-    }
-    public static String[] randomStringArr(int num, int length) {
-        String[] arr = new String[num];
-        while (num > 0) {
-            int i = 0;
-            String s = "";
-            while (i < length) {
-                char c = (char)((Math.random()*26)+97);
-                s = s + c;
                 i++;
+                swap(arr, i, j);
             }
-            num--;
-            arr[num] = s;
         }
-        return arr;
+        swap(arr,i+1, right);
+        return i + 1;
     }
 
-    public static int[] randInt(int count) {
-        int[] arr = new int[count];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int)(Math.random()*10000);
+    public static void quickSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivot = partition(arr, left, right);
+
+            quickSort(arr,left, pivot -1);
+            quickSort(arr, pivot + 1, right);
+
         }
-        return arr;
     }
 
-    public static double[] randDouble(int count) {
-        double[] arr = new double[count];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Math.random()*10000;
-        }
-        return arr;
-    }
-
-    }
-
+}
 
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -3,6 +3,37 @@ package com.company;
 public class Runner {
 
     public static void main(String[] args) {
-	// write your code here
+
+        InPlaceSorts y = new InPlaceSorts();
+
+        int[] intarr = y.randomIntArr(10);
+        double[] Doublearr = y.IntToDouble(intarr);
+        String[]  Stringarr = y.IntToString(intarr);
+
+
+        long time = System.nanoTime();
+        y.insertionSort(intarr);
+        time = System.nanoTime() - time;
+        System.out.println("Time Taken - Insertion Sort: "+ time);
+
+        long time2 = System.nanoTime();
+        y.selectionSort(Doublearr);
+        time2 = System.nanoTime() - time2;
+        System.out.println("Time Taken - Selection Sort: "+ time2);
+
+
+        long time3 = System.nanoTime();
+        y.bubbleSort(Stringarr);
+        time3 = System.nanoTime() - time3;
+        System.out.println("Time Taken - Bubble Sort: "+ time3);
+
+        int[] ekor = {10, 23, 3 ,4 ,6};
+        InPlaceSorts.quickSort(ekor, 0, ekor.length-1);
+        System.out.println(ekor[0]);
+        System.out.println(ekor[1]);
+        System.out.println(ekor[2]);
+        System.out.println(ekor[3]);
+        System.out.println(ekor[4]);
+
     }
 }
